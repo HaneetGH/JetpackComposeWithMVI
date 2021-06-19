550b62c9fcb660e6f09e62c5dfdb10269c96f182
haneetsinghchhabra <HOTh@n!!t52p93>
2021-06-20 02:38:30 +0530

    First Column

--- app/src/main/java/com/technorapper/jetpackcomposewithmvi/MainActivity.kt
+++ app/src/main/java/com/technorapper/jetpackcomposewithmvi/MainActivity.kt
@@ -3,15 +3,16 @@ package com.technorapper.jetpackcomposewithmvi
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.technorapper.jetpackcomposewithmvi.ui.theme.JetpackComposeWithMVITheme
@@ -19,7 +20,7 @@ import com.technorapper.jetpackcomposewithmvi.ui.theme.JetpackComposeWithMVIThem
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContent { DefaultPreview() }
     }
 }
 
@@ -34,16 +35,31 @@ fun DefaultPreview() {
     JetpackComposeWithMVITheme {
         //Greeting("Android")
 
-        Column(modifier = Modifier.padding(16.dp)) {
+        /*Column(modifier = Modifier.padding(16.dp)) {
             Text(text = "Hello World")
             Spacer(modifier  = Modifier.padding(top =5.dp))
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { *//*TODO*//* }) {
 
                 Text(text = "click here")
 
             }
 
-        }
-            
+        }*/
+        Column() {
+
+
+            Image(
+                painterResource(R.drawable.happy_meal),
+                contentDescription = ""
+            )
+
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Happy Meal")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Text(text = "800 cal")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Text(text = "$5.99")
+            }
         }
     }
+}
