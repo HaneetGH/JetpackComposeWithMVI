452661f2d21f178afa35f7fd03674b197f0f021a
haneetsinghchhabra <HOTh@n!!t52p93>
2021-06-20 03:19:07 +0530

    color

--- app/src/main/java/com/technorapper/jetpackcomposewithmvi/MainActivity.kt
+++ app/src/main/java/com/technorapper/jetpackcomposewithmvi/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.technorapper.jetpackcomposewithmvi
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.technorapper.jetpackcomposewithmvi.ui.theme.JetpackComposeWithMVITheme
 
 class MainActivity : ComponentActivity() {
@@ -45,20 +56,34 @@ fun DefaultPreview() {
             }
 
         }*/
-        Column() {
+       Column(modifier = Modifier.background(androidx.compose.ui.graphics.Color.Gray)/*,Modifier.verticalScroll(rememberScrollState())*/) {
 
 
             Image(
                 painterResource(R.drawable.happy_meal),
-                contentDescription = ""
+                contentDescription = "",
+                modifier = Modifier.height(300.dp),
+                contentScale= ContentScale.Crop,
             )
 
             Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = "Happy Meal")
+                Text(text = "Happy Meal",
+                style = TextStyle(
+                    fontSize = 26.sp
+                )
+                )
                 Spacer(modifier = Modifier.padding(top = 10.dp))
-                Text(text = "800 cal")
+                Text(text = "800 cal"
+                    ,
+                    style = TextStyle(
+                        fontSize = 17.sp
+                    ))
                 Spacer(modifier = Modifier.padding(top = 10.dp))
-                Text(text = "$5.99")
+                Text(text = "$5.99",
+                    style = TextStyle(
+                        color= androidx.compose.ui.graphics.Color.Green,
+                        fontSize = 17.sp
+                    ))
             }
         }
     }
